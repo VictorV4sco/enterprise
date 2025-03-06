8807eb068848ecf72b677727c47fc4febbce415d
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-06 10:52:24 -0300

    Validations implementation

--- src/main/java/com/enterprise/dto/EmployeeDTO.java
+++ src/main/java/com/enterprise/dto/EmployeeDTO.java
@@ -2,16 +2,30 @@ package com.enterprise.dto;
 
 import com.enterprise.entities.JobPosition;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record EmployeeDTO(
 		Long id, 
+		@NotBlank(message = "Campo requer preenchimento")
 		String name, 
+		
+		@Positive
+		@NotNull
 		Double salary, 
+		
+		@NotBlank(message = "Campo requer preenchimento")
 		String hiredDate, 
 		String dismissalDate,
+		
+		@NotBlank(message = "Campo requer preenchimento")
 		JobPosition jobPosition, 
 		String state, 
 		String city, 
 		String neiborhood, 
 		String address, 
+		
+		@NotBlank(message = "Campo requer preenchimento")
 		Long zipCode
 		) {}
