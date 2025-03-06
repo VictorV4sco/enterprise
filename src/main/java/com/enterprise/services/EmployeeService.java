1d007a6b1741cd9dfc2fffb9eb4a46598e4d98d9
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-06 11:19:16 -0300

    Error implementation

--- src/main/java/com/enterprise/services/EmployeeService.java
+++ src/main/java/com/enterprise/services/EmployeeService.java
@@ -5,11 +5,13 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.enterprise.dto.AllEmployeesDTO;
 import com.enterprise.dto.EmployeeDTO;
 import com.enterprise.entities.Employee;
 import com.enterprise.repositories.EmployeeRepository;
+import com.enterprise.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class EmployeeService {
@@ -17,41 +19,30 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository repository;
 	
-	public EmployeeDTO findById(Long id) throws Exception {
-		try {
-			Employee employee = repository.findById(id).orElseThrow(() -> new Exception("Id" + id + "not found"));
-			return new 
-					EmployeeDTO(
-							employee.getIdEmployee(),
-							employee.getName(),
-							employee.getSalary(),
-							employee.getHiredDate(), 
-							employee.getDismissalDate(), 
-							employee.getJobPosition(), 
-							employee.getState(),
-							employee.getCity(),
-							employee.getNeiborhood(), 
-							employee.getAddress(), 
-							employee.getZipCode()
+	@Transactional(readOnly = true)
+	public EmployeeDTO findById(Long id) {
+		
+		Employee employee = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+		return new 
+				EmployeeDTO(
+						employee.getIdEmployee(),
+						employee.getName(),
+						employee.getSalary(),
+						employee.getHiredDate(), 
+						employee.getDismissalDate(), 
+						employee.getJobPosition(), 
+						employee.getState(),
+						employee.getCity(),
+						employee.getNeiborhood(), 
+						employee.getAddress(), 
+						employee.getZipCode()
 							);
-		} catch (Exception e) {
-			throw e;
-		}
 	}
 	
-	public List<AllEmployeesDTO> findAllEmployees() throws Exception {
-		try {
-			List<Employee> employees = repository.findAll();
-			
-			if (employees.isEmpty()) {
-				throw new Exception("Employees not found");
-			}
-			
-			return employees.stream().map(employee -> new AllEmployeesDTO(employee.getIdEmployee(), employee.getName(),
-					employee.getHiredDate(), employee.getDismissalDate(), employee.getJobPosition())).collect(Collectors.toList());
-			
-		} catch (Exception e) {
-			throw e;
-		}
+	public List<AllEmployeesDTO> findAllEmployees() {
+		
+		List<Employee> employees = repository.findAll();
+		return employees.stream().map(employee -> new AllEmployeesDTO(employee.getIdEmployee(), employee.getName(),
+				employee.getHiredDate(), employee.getDismissalDate(), employee.getJobPosition())).collect(Collectors.toList());
 	}
 }
